a132a989db8200c5419c6ac28ee6d5b51435562c
morethanmin <mini4614@gmail.com>
2021-03-26 00:03:48 +0900

    tdd

---
+++ velopertModernReact/Chaptor06/learn-redux/src/modules/todos.js
@@ -0,0 +1,42 @@
+// 액션 타입 선언
+const ADD_TODO = "todos/ADD_TODO";
+const TOGGLE_TODO = "todos/TOGGLE_TODO";
+
+// 액션 생성함수 선언
+let nextId = 1; // todo 데이터에서 사용할 고유 id
+export const addTodo = (text) => ({
+  type: ADD_TODO, // 새 항목을 추가하고 nextId 값에 1을 더해줍니다.
+  todo: { id: nextId++, text },
+});
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  id,
+});
+
+// 초기 상태 선언
+// 리듀서의 초기 상태는 꼭 갹체타입일 필요 없습니다.
+// 배열이여도 되고 원시 타입 이여도 상관 없습니다.
+const initialState = [
+  /* 우리는 다음과 같이 구성된 객체를 이 배열 안에 넣을 것입니다.
+    {
+        id:1,
+        text: 'text',
+        done: false
+    }
+    */
+];
+
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat(action.todo);
+
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+
+    default:
+      return state;
+  }
+}
