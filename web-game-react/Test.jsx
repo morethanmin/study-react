c9255c547ba9261812c319141e0dbcd004a63cc8
morethanmin <mini4614@gmail.com>
2021-08-19 22:28:47 +0900

    til

---
+++ web-game-react/Test.jsx
@@ -0,0 +1,9 @@
+const React = require('react')
+const { Component } = React
+class Test extends Component {
+  render() {
+    return <div>hellfdsfsfdfso!</div>
+  }
+}
+
+module.exports = Test
---
+++ web-game-react/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react')
+const ReactDom = require('react-dom')
+
+const Test = require('./Test')
+
+ReactDom.render(<Test />, document.querySelector('#root'))
